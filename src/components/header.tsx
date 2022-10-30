db0211382835987e29f52d4300f4793d45f28000
Faisal <saleemfs27@gmail.com>
2022-10-30 19:55:02 +0200

    Header refactoring

---
+++ src/components/header.tsx
@@ -0,0 +1,162 @@
+import Image from 'next/future/image'
+import { signOut } from 'next-auth/react'
+import { Dispatch, SetStateAction, useEffect, useState } from 'react'
+import { List } from '@prisma/client'
+
+import s from '@/styles/header.module.scss'
+import { Session } from 'next-auth'
+
+type ListWithIcon = List & { icon: string }
+
+type GetListsResponse = {
+  success: boolean
+  data: ListWithIcon[]
+  errors: unknown
+}
+
+type HeaderProps = {
+  status: 'loading' | 'authenticated' | 'unauthenticated'
+  session: Session | null
+  lists: GetListsResponse | undefined
+  activeListId: number
+  setActiveListId: Dispatch<SetStateAction<number>>
+  activeList: ListWithIcon | undefined
+  setActiveList: Dispatch<SetStateAction<ListWithIcon | undefined>>
+  setShowCreateList: Dispatch<SetStateAction<boolean>>
+}
+
+const Header = ({
+  status,
+  session,
+  lists,
+  activeListId,
+  setActiveListId,
+  activeList,
+  setActiveList,
+  setShowCreateList,
+}: HeaderProps) => {
+  const [showList, setShowList] = useState(false)
+
+  useEffect(() => {
+    setActiveList(lists?.data.find((list) => list.id === activeListId))
+  }, [activeListId, lists?.data, setActiveList])
+
+  return (
+    <header className={s.header}>
+      <div className={s.listContainer}>
+        {lists && lists.data && lists.data[0] && activeList && (
+          <div className={s.selectWrapper}>
+            <div
+              className={`${s.select} ${showList && s.isActive}`}
+              onClick={() => setShowList(!showList)}
+            >
+              <>
+                <div className={s.selectTrigger}>
+                  <div className={s.selectedList}>
+                    <Image
+                      src={activeList.icon}
+                      alt='icon'
+                      width={20}
+                      height={20}
+                      className={s.icon}
+                    />
+                    <div className={s.text}>
+                      {activeList.type.charAt(0) +
+                        activeList.type.slice(1).toLowerCase()}
+                    </div>
+                  </div>
+                  <div className={s.selectIcon}>
+                    <Image
+                      src='/arrow.svg'
+                      alt='arrow'
+                      width={20}
+                      height={20}
+                    />
+                  </div>
+                </div>
+                <div className={s.selectOptions}>
+                  {lists.data.map((list, idx: number) => {
+                    return (
+                      <div
+                        key={idx}
+                        className={s.selectOption}
+                        onClick={() => setActiveListId(list.id)}
+                      >
+                        <div className={s.icon}>
+                          <Image
+                            src={list.icon}
+                            alt='icon'
+                            width={20}
+                            height={20}
+                          />
+                        </div>
+                        <div className={s.text}>
+                          {list.type.charAt(0) +
+                            list.type.slice(1).toLowerCase()}
+                        </div>
+                      </div>
+                    )
+                  })}
+                </div>
+              </>
+            </div>
+          </div>
+        )}
+        <Image
+          src='/plus.svg'
+          alt='addList'
+          width={50}
+          height={50}
+          title='Create list'
+          className={s.addIcon}
+          onClick={() => setShowCreateList(true)}
+        />
+      </div>
+      <div className={s.logoContainer}>
+        <Image
+          src='/logo.svg'
+          alt='logo'
+          width={150}
+          height={40}
+          className={s.logo}
+        />
+      </div>
+      <div className={s.profileContainer}>
+        {status === 'loading' && (
+          <Image
+            src='/loader.svg'
+            alt='loader'
+            width={50}
+            height={50}
+            className={s.loader}
+          />
+        )}
+        {status === 'authenticated' && (
+          <div className={s.profileContainer}>
+            <div className={s.profile}>
+              <div className={s.name}>{session?.user?.name}</div>
+              <div className={s.dpContainer}>
+                {session?.user?.image && (
+                  <Image
+                    src={session.user?.image}
+                    alt='dp'
+                    width={100}
+                    height={100}
+                    className={s.dp}
+                  />
+                )}
+              </div>
+            </div>
+            <div
+              className={s.logout}
+              onClick={() => signOut({ callbackUrl: '/' })}
+            >
+              Logout
+            </div>
+          </div>
+        )}
+      </div>
+    </header>
+  )
+}
+export default Header
--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,22 +1,21 @@
 import { Item, List } from '@prisma/client'
 import { QueryFunctionContext, useQuery } from '@tanstack/react-query'
 import axios from 'axios'
-import { GetServerSideProps, InferGetServerSidePropsType } from 'next'
-import { signOut, useSession } from 'next-auth/react'
-import Image from 'next/future/image'
+import { useSession } from 'next-auth/react'
 import Head from 'next/head'
 import { useEffect, useRef, useState } from 'react'
 import { toast } from 'react-toastify'
 
 import CreateList from '@/components/create-list'
 import TodoCard from '@/components/todo-card'
-import { getServerAuthSession } from '@/server/common/get-server-auth-session'
-import { prisma } from '@/server/db/client'
 import s from '@/styles/home.module.scss'
 import { B, col, G, R } from '@/utils/animation'
 import CreateItem from '@/components/create-item'
+import Header from '@/components/header'
 
-const SPEED = 0.05
+const ANIMATION_SPEED = 0.05
+
+type ListWithIcon = List & { icon: string }
 
 type GetListsResponse = {
   success: boolean
@@ -42,10 +41,9 @@ const fetchItems = async ({
   return (await axios.post('/api/item/get', body)).data
 }
 
-const Home = ({}: InferGetServerSidePropsType<typeof getServerSideProps>) => {
+const Home = () => {
   const { data: session, status } = useSession()
   const [showCreateList, setShowCreateList] = useState(false)
-  const [showList, setShowList] = useState(false)
   const [activeListId, setActiveListId] = useState(1)
   const [activeList, setActiveList] = useState<ListWithIcon>()
   const canvasRef = useRef<HTMLCanvasElement>(null)
@@ -68,7 +66,7 @@ const Home = ({}: InferGetServerSidePropsType<typeof getServerSideProps>) => {
             col(ctx, x, y, R(x, y, t), G(x, y, t), B(x, y, t))
           }
         }
-        t = t + SPEED
+        t = t + ANIMATION_SPEED
         window.requestAnimationFrame(animate)
       }
 
@@ -101,10 +99,6 @@ const Home = ({}: InferGetServerSidePropsType<typeof getServerSideProps>) => {
     }
   )
 
-  useEffect(() => {
-    setActiveList(lists?.data.find((list) => list.id === activeListId))
-  }, [activeListId, lists?.data])
-
   const { data: items } = useQuery<GetItemsResponse, Error>(
     ['items', { listId: activeListId }],
     fetchItems,
@@ -132,121 +126,16 @@ const Home = ({}: InferGetServerSidePropsType<typeof getServerSideProps>) => {
       />
       <div className={s.hero}>
         <canvas ref={canvasRef} width='32px' height='32px' />
-        <header className={s.header}>
-          <div className={s.listContainer}>
-            {lists && lists.data && lists.data[0] && activeList && (
-              <div className={s.selectWrapper}>
-                <div
-                  className={`${s.select} ${showList && s.isActive}`}
-                  onClick={() => setShowList(!showList)}
-                >
-                  <>
-                    <div className={s.selectTrigger}>
-                      <div className={s.selectedList}>
-                        <Image
-                          src={activeList.icon}
-                          alt='icon'
-                          width={20}
-                          height={20}
-                          className={s.icon}
-                        />
-                        <div className={s.text}>
-                          {activeList.type.charAt(0) +
-                            activeList.type.slice(1).toLowerCase()}
-                        </div>
-                      </div>
-                      <div className={s.selectIcon}>
-                        <Image
-                          src='/arrow.svg'
-                          alt='arrow'
-                          width={20}
-                          height={20}
-                        />
-                      </div>
-                    </div>
-                    <div className={s.selectOptions}>
-                      {lists.data.map((list, idx: number) => {
-                        return (
-                          <div
-                            key={idx}
-                            className={s.selectOption}
-                            onClick={() => setActiveListId(list.id)}
-                          >
-                            <div className={s.icon}>
-                              <Image
-                                src={list.icon}
-                                alt='icon'
-                                width={20}
-                                height={20}
-                              />
-                            </div>
-                            <div className={s.text}>
-                              {list.type.charAt(0) +
-                                list.type.slice(1).toLowerCase()}
-                            </div>
-                          </div>
-                        )
-                      })}
-                    </div>
-                  </>
-                </div>
-              </div>
-            )}
-            <Image
-              src='/plus.svg'
-              alt='addList'
-              width={50}
-              height={50}
-              title='Create list'
-              className={s.addIcon}
-              onClick={() => setShowCreateList(true)}
-            />
-          </div>
-          <div className={s.logoContainer}>
-            <Image
-              src='/logo.svg'
-              alt='logo'
-              width={150}
-              height={40}
-              className={s.logo}
-            />
-          </div>
-          <div className={s.profileContainer}>
-            {status === 'loading' && (
-              <Image
-                src='/loader.svg'
-                alt='loader'
-                width={50}
-                height={50}
-                className={s.loader}
-              />
-            )}
-            {status === 'authenticated' && (
-              <div className={s.profileContainer}>
-                <div className={s.profile}>
-                  <div className={s.name}>{session.user?.name}</div>
-                  <div className={s.dpContainer}>
-                    {session.user?.image && (
-                      <Image
-                        src={session.user?.image}
-                        alt='dp'
-                        width={100}
-                        height={100}
-                        className={s.dp}
-                      />
-                    )}
-                  </div>
-                </div>
-                <div
-                  className={s.logout}
-                  onClick={() => signOut({ callbackUrl: '/' })}
-                >
-                  Logout
-                </div>
-              </div>
-            )}
-          </div>
-        </header>
+        <Header
+          status={status}
+          session={session}
+          lists={lists}
+          activeListId={activeListId}
+          setActiveListId={setActiveListId}
+          activeList={activeList}
+          setActiveList={setActiveList}
+          setShowCreateList={setShowCreateList}
+        />
         <CreateItem activeListId={activeListId} />
       </div>
       <main className={s.main}>
@@ -269,65 +158,3 @@ const Home = ({}: InferGetServerSidePropsType<typeof getServerSideProps>) => {
   )
 }
 export default Home
-
-type ListWithIcon = List & { icon: string }
-
-type Props = {
-  lists: ListWithIcon[]
-  items: Item[]
-}
-
-export const getServerSideProps: GetServerSideProps<Props> = async ({
-  req,
-  res,
-}) => {
-  const session = await getServerAuthSession({ req, res })
-
-  if (!session) {
-    return {
-      redirect: {
-        destination: '/',
-        permanent: false,
-      },
-    }
-  }
-
-  // get all lists
-  const lists = await prisma.list.findMany({
-    where: { userId: session.user?.id },
-  })
-
-  const listsWithIcon: ListWithIcon[] = []
-  lists.forEach((list) => {
-    listsWithIcon.push({
-      ...list,
-      icon:
-        list.type === 'SPORTS'
-          ? '/sports.svg'
-          : list.type === 'WORK'
-          ? '/work.svg'
-          : list.type === 'SHOPPING'
-          ? '/shopping.svg'
-          : list.type === 'TRAVEL'
-          ? '/travel.svg'
-          : list.type === 'OTHER'
-          ? '/other.svg'
-          : '/other.svg',
-    })
-  })
-
-  // get all items
-  let items = Array<Item>()
-  if (lists.length > 0) {
-    items = await prisma.item.findMany({
-      where: { listId: lists[0]?.id },
-    })
-  }
-
-  return {
-    props: {
-      lists: listsWithIcon,
-      items,
-    },
-  }
-}
